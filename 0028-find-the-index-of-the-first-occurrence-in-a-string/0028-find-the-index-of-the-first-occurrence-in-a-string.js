aadc3214e5a08b426803588509c1c2486649adb1
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-03-31 18:27:49 +0900

    Time: 60 ms (18.56%), Space: 48.4 MB (91.28%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function(haystack, needle) {
+  const isIndex = haystack.indexOf(needle);
+  
+  if(isIndex === -1) {
+    return isIndex;
+  } else {
+    return isIndex;
+  }
+};
