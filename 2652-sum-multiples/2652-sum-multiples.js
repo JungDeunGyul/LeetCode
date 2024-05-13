6dd8e6a20b691521a71b75ab7a88198f429521c5
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-05-13 19:20:07 +0900

    Time: 77 ms (41.10%), Space: 52.6 MB (74.89%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -6,7 +6,11 @@ const sumOfMultiples = function(n) {
   let sumNumber = 0;
   
   for (let i = 0; i <= n; i++) {
-    if (i % 3 === 0 || i % 5 ===0 || i % 7 === 0) {
+    if (i % 3 === 0) {
+      sumNumber += i;
+    } else if (i % 5 === 0) {
+      sumNumber += i;
+    } else if (i % 7 ===0) {
       sumNumber += i;
     }
   }
