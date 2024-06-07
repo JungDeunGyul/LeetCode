b61b6c013498f369a47d8cf4dc879f6417833618
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-06-07 15:23:47 +0900

    Time: 65 ms (20.63%), Space: 49.6 MB (56.95%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+const relativeSortArray = function(arr1, arr2) {
+  const result = [];
+  const arr1Length = arr1.length;
+  const arr2Length = arr2.length;
+  
+  for (let i = 0; i < arr2Length; i++) {
+    for (let k = 0; k < arr1Length; k++) {
+      if (arr2[i] === arr1[k]) {
+        result.push(arr1[k]);
+        arr1.splice(k, 1);
+        k--;
+      }
+    }
+  }
+  arr1.sort((a,b) => a - b);
+  
+  result.push(...arr1);
+  
+  return result;
+};
