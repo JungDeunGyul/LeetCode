4b11ab269d58bb3fe9a16223b6e6f685f6cee8e3
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-06-23 16:33:40 +0900

    Time: 63 ms (42.95%), Space: 52.1 MB (19.29%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+  if (root === null) {
+    return 0;
+  }
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+  
+  const result = Math.max(leftDepth, rightDepth) + 1;
+
+  return result;
+};
