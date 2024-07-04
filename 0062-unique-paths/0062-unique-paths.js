216c658749a44be8210e834c676e2138f20cb251
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-07-05 00:50:41 +0900

    Time: 58 ms (35.89%), Space: 48.9 MB (72.32%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const uniquePaths = function(m, n) {
+  const dp = Array.from({ length: m }, () => Array(n).fill(1));
+  const width = n;
+  const length = m;
+
+  for (let i = 1; i < length; i++) {
+    for (let j = 1; j < width; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[length - 1][width - 1];
+};
