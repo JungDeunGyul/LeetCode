d40f98455ae23d61a822f96feccc2ca3ac2367eb
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-08-11 21:32:40 +0900

    Time: 53 ms (59.10%), Space: 49.2 MB (21.35%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors = function(nums) {
+  const sortNums = nums.sort();
+  
+  return sortNums; 
+};
