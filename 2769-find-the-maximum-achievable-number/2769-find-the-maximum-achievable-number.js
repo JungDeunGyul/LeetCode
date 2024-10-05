98bbeed100060f1f6936b1839976ad9e74f8bb6a
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-10-06 00:33:22 +0900

    Time: 91 ms (64.84%), Space: 53.6 MB (64.22%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+const theMaximumAchievableX = function(num, t) {
+  const result = num + 2 * t;
+  
+  return result;
+};
