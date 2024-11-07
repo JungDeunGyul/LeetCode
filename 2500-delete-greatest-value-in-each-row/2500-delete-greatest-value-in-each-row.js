3628c8d031fd4e1351fb111ed9524b18abec8604
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-11-07 19:45:41 +0900

    Time: 13 ms (28.57%), Space: 51.5 MB (64.29%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const deleteGreatestValue = function (grid) {
+  let sumMaximumNum = 0;
+
+  while (grid[0].length > 0) {
+    let maxInThisRound = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+      let maxInRow = Math.max(...grid[i]);
+      grid[i].splice(grid[i].indexOf(maxInRow), 1);
+
+      if (maxInRow > maxInThisRound) {
+        maxInThisRound = maxInRow;
+      }
+    }
+    
+    sumMaximumNum += maxInThisRound;
+  }
+
+  return sumMaximumNum;
+};
