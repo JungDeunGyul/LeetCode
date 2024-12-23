eeaaa9545d9b1505aa9bafb14db3425ba5330606
JungDeunGyul <112459919+JungDeunGyul@users.noreply.github.com>
2024-12-23 19:10:24 +0900

    Time: 0 ms (100%), Space: 49.8 MB (78.53%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countGoodSubstrings = function(s) {
+  let result = 0;
+
+  for (let i = 0; i < s.length - 2; i++) {
+    if (
+      s[i] !== s[i + 1] &&
+      s[i] !== s[i + 2] &&
+      s[i + 1] !== s[i + 2]
+      ) {
+      result++;
+    }
+  }
+  
+  return result;
+};
